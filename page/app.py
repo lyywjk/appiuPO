d11034e9c69d23ecb6093fb144997cfe6c5ec9c3
liyaya <1564088177@qq.com>
2020-11-26 15:58:30 +0800

    first commit

---
+++ page/app.py
@@ -0,0 +1,28 @@
+from appium import webdriver
+from appium.webdriver.webdriver import WebDriver
+
+from PO_lianx.page.main import MianPage
+
+
+class App:
+    driver: WebDriver
+
+    def start(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        # desired_caps['platformVersion'] = '6.0'
+        desired_caps['deviceName'] = 'emulator-5554'
+        desired_caps['appPackage'] = 'com.tencent.wework'
+        desired_caps['appActivity'] = '.launch.WwMainActivity'
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+        self.driver.implicitly_wait(10)
+        return self
+
+    def restart(self):
+        pass
+
+    def close(self):
+        pass
+
+    def main(self):
+        return MianPage(self.driver)
